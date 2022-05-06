8667e4cac010d22dae2aef6f0f72a98bf7c3be2a
Erwin Desktop <johndoe@example.com>
2022-05-07 00:33:50 +0800

    added cursor tracking

--- static/app.js
+++ static/app.js
@@ -12,16 +12,33 @@ function degToRad(degrees) {
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
-var x = 50;
-var y = 50;
+var x_0 = Math.floor(canvas.width / 2) + 0.5;
+var y_0 = Math.floor(canvas.height / 2) + 0.5;
+var x = x_0;
+var y = y_0;
 
 function canvasDraw() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = "black";
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#f00";
   ctx.beginPath();
   ctx.arc(x, y, RADIUS, 0, degToRad(360), true);
   ctx.fill();
+
+  // remove aliasing
+  ctx.strokeWidth = 1;
+  ctx.moveTo(x_0, y_0 - 10);
+  ctx.lineTo(x_0, y_0 + 10);
+  ctx.moveTo(x_0 - 10,  y_0);
+  ctx.lineTo(x_0 + 10,  y_0);
+  // Line color
+  ctx.strokeStyle = 'green';
+  ctx.stroke();
+  ctx.moveTo(x_0,  y_0);
+  ctx.lineTo(x ,  y);
+  ctx.strokeStyle = 'green';
+  ctx.stroke();
 }
 canvasDraw();
 
@@ -60,18 +77,22 @@ var animation;
 function updatePosition(e) {
   x += e.movementX;
   y += e.movementY;
-  if (x > canvas.width + RADIUS) {
-    x = -RADIUS;
+  if (x > canvas.width) {
+    x = canvas.width;
   }
-  if (y > canvas.height + RADIUS) {
-    y = -RADIUS;
+  if (y > canvas.height) {
+    y = canvas.height;
   }  
-  if (x < -RADIUS) {
-    x = canvas.width + RADIUS;
+  if (x < 0) {
+    x = 0;
   }
-  if (y < -RADIUS) {
-    y = canvas.height + RADIUS;
+  if (y < 0) {
+    y = 0;
   }
+  redrawTracker()
+}
+
+function redrawTracker() {
   tracker.textContent = "X position: " + x + ", Y position: " + y;
 
   if (!animation) {
@@ -81,3 +102,27 @@ function updatePosition(e) {
     });
   }
 }
+
+var h_move = 2; // 2 px movement
+function loop() {
+  if (!(x == x_0 && y == y_0)) {
+    // Convert to cartesian with center 0,0
+    var x_rel = x - x_0
+    var y_rel = y - y_0
+    var h = Math.hypot(x_rel, y_rel)
+    if (h_move > h) { // Close to origin case
+      x = x_0
+      y = y_0
+    }
+    else {
+      var h_move_delta = h - h_move
+      x = x_rel/h*h_move_delta + x_0
+      y = y_rel/h*h_move_delta + y_0
+    }
+    console.log("x: "+x+" y: "+y)
+    // execute loop function over and over
+    redrawTracker();
+  }
+  requestAnimationFrame(loop);
+}
+loop()
